c34189001173dd0add3915a42c6719779b7c8702
Jordan Gilbert <jordan.gilbert@hotmail.co.uk>
2025-05-16 10:23:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -86,3 +86,21 @@
 
 
 }());
+
+// Hide/show top bar on scroll
+let lastScrollTop = 0;
+const topBar = document.getElementById('topBar');
+
+window.addEventListener('scroll', function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    topBar.style.top = "-70px";
+  } else {
+    // Scrolling up
+    topBar.style.top = "0";
+  }
+  
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
